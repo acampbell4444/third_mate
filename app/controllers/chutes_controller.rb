310f4f7072359c13ee78b921315e17d4fb7d7d36
Alan Campbell <alancampbell4444@gmail.com>
2016-05-16 23:19:15 -0700

    chutesform and chute model

--- app/controllers/chutes_controller.rb
+++ app/controllers/chutes_controller.rb
@@ -5,6 +5,28 @@ class ChutesController < ApplicationController
 
   def new
     @chute = Chute.new
-
   end
+
+  def create
+    @chute = Chute.create
+    @chute = Chute.create(create_params)
+
+    if @chute.save
+      flash[:notice] = 'Info submitted.'
+      redirect_to chute_path(@chute)
+    else
+      flash.now[:alert] = 'There was an error saving the info. Please try again.'
+       render new_chute_path
+    end
+  end
+
+  def show
+    @chute = Chute.find(params[:id])
+  end
+
+  def create_params
+    params.require(:chute).permit(:turbulence, :windspeed, :min_wt, :max_wt)
+  end
+
+
 end
