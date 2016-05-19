2a1edc55809f2c1a924eca96986a03b3e54c1552
Alan Campbell <alancampbell4444@gmail.com>
2016-05-19 13:28:57 -0700

    second chute added

--- app/models/chute.rb
+++ app/models/chute.rb
@@ -43,7 +43,52 @@ class Chute < ActiveRecord::Base
     elsif windspeed > 17
       return "According to the Manufacturer, It is Too windy to fly a 35 with the Zippers Open"
     else
-      return eagle[windspeed]
+      return tristar[windspeed]
+    end
+  end
+
+  def tristar_37_closed(windspeed)
+    tristar = {
+      0 =>  [120, 410],
+      1 =>  [120, 430],
+      2 =>  [120, 455],
+      3 =>  [120, 475],
+      4 =>  [120, 500],
+      5 =>  [120, 525],
+      6 =>  [120, 525],
+      7 =>  [120, 525],
+      8 =>  [120, 525],
+      9 =>  [120, 525],
+      10 => [200, 525],
+      11 => [275, 525],
+      12 => [360, 525],
+      13 => [450, 525],
+      14 => [475, 525]
+    }
+    if windspeed > 14
+      return "According to the Manufacturer, It is Too windy to fly a 37 with the Zippers Closed."
+    else
+      return tristar[windspeed]
+    end
+  end
+
+  def tristar_37_open(windspeed)
+    tristar = {
+      9  => [120, 120],
+      10 => [120, 195],
+      11 => [120, 275],
+      12 => [200, 355],
+      13 => [280, 440],
+      14 => [360, 520],
+      15 => [445, 525],
+      16 => [525, 525]
+    }
+    if windspeed < 9
+      return "It is not necessary to unzip the 37 until winds reach at least 9 mph"
+    elsif windspeed > 16
+      return "According to the Manufacturer, It is Too windy to fly a 37 with the Zippers Open"
+    else
+      return tristar[windspeed]
     end
   end
 
